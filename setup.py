61fb0009635882b1a6a09636e6a6b7d8e89a3ba0
lix <lix_xu@jabil.com>
2015-03-11 16:31:10 +0800

    shift minute can be customized

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='jshdate',
-    version='0.1.1',
+    version='0.1.9',
     url='https://github.com/lixxu/jshdate',
     license='BSD',
     author='Lix Xu',
